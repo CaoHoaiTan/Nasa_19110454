62199844a89a3f6b54c10d8fc51e47109d76d09c
Cao Hoài Tấn <caohoaitan@gmail.com>
2022-11-23 09:51:03 +0700

    fix and add test

--- server/src/test/lauches.test.js
+++ server/src/test/lauches.test.js
@@ -1,58 +1,92 @@
 const app = require('../app');
 const request = require('supertest');
+const {
+    connect,
+    close
+} = require('../services/mongo');
 
-describe('GET /lauches', function () {
-    it('responds with json', function () {
-        return request(app)
-            .get('/launches/')
-            .set('Accept', 'application/json')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then(response => {
-                expect(response.body).toBeInstanceOf(Array);
-            })
+const {
+    loadPlanetsData,
+} = require('../models/planets.model');
+
+describe('Launches API', () => {
+    beforeAll(async () => {
+        await connect();
+        await loadPlanetsData();
     });
-});
 
-describe('POST /launches', function () {
-    it('responds with success', function () {
-        return request(app)
-            .post('/launches/')
-            .send({
-                mission: 'USS Enterprise',
-                rocket: 'NCC-1701-D',
-                target: 'Kepler-186 f',
-                launchDate: 'October 31, 2022',
-            })
-            .expect(201)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    mission: 'USS Enterprise',
-                    rocket: 'NCC-1701-D',
-                    target: 'Kepler-186 f',
-                    launchDate: '2022-10-30T17:00:00.000Z',
-                    upcoming: true,
-                    success: true,
-                });
+    afterAll(async () => {
+        await close();
+    });
+
+    describe('Test GET /launches', () => {
+        test('It should respond with 200 success', async () => {
+            const response = await request(app)
+                .get('/launches')
+                .expect('Content-Type', /json/)
+                .expect(200);
+        });
+    });
+
+    describe('Test POST /launches', () => {
+        const completeLaunchData = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC-1701-D',
+            target: 'Kepler-1649 b',
+            launchDate: 'January 4, 2028',
+        };
+
+        const launchDataWithoutDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC-1701-D',
+            target: 'Kepler-1649 b',
+        };
+
+        const launchDataWithInvalidDate = {
+            mission: 'USS Enterprise',
+            rocket: 'NCC-1701-D',
+            target: 'Kepler-1649 b',
+            launchDate: 'zoot',
+        };
+
+        test('It should respond with 201 created', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(completeLaunchData)
+                .expect('Content-Type', /json/)
+                .expect(201);
+
+            const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+            const responseDate = new Date(response.body.launchDate).valueOf();
+            expect(responseDate).toBe(requestDate);
+
+            expect(response.body).toMatchObject(launchDataWithoutDate);
+        });
+
+        test('It should catch missing required properties', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithoutDate)
+                .expect('Content-Type', /json/)
+                .expect(400);
+
+            expect(response.body).toStrictEqual({
+                error: 'Missing required launch property',
             });
-    });
-});
 
-describe('DELETE /launches', function () {
-    it('responds with success', function () {
-        return request(app)
-            .delete('/launches/101')
-            .expect(200)
-            .then(response => {
-                expect(response.body).toMatchObject({
-                    mission: 'USS Enterprise',
-                    rocket: 'NCC-1701-D',
-                    target: 'Kepler-186 f',
-                    launchDate: '2022-10-30T17:00:00.000Z',
-                    upcoming: false,
-                    success: false,
-                });
+        });
+
+        test('It should catch invalid dates', async () => {
+            const response = await request(app)
+                .post('/launches')
+                .send(launchDataWithInvalidDate)
+                .expect('Content-Type', /json/)
+                .expect(400);
+
+            expect(response.body).toStrictEqual({
+                error: 'Invalid launch date',
             });
+
+        });
     });
 });
-
--- server/src/test/planets.test.js
+++ server/src/test/planets.test.js
@@ -1,12 +1,22 @@
 const app = require('../app');
 const request = require('supertest');
+const {
+    connect,
+    close
+} = require('../services/mongo');
+
 
 describe('GET /planets', function () {
+    beforeAll(async () => {
+        await connect();
+    });
+
+    afterAll(async () => {
+        await close();
+    });
     it('responds with json', async function () {
         const response = await request(app)
             .get('/planets/')
-            .set('Accept', 'application/json')
-        expect(response.headers['content-type']).toEqual(expect.stringContaining("json"));
         expect(response.status).toEqual(200);
     });
 });
